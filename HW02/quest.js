5b7887b17afea35b8e3cedbeaab67df9d596d35d
dogdevelophoyri <hyori7272@gmail.com>
2026-05-05 13:32:29 +0900

    과제

--- HW02/quest.js
+++ HW02/quest.js
@@ -15,16 +15,16 @@ const arr2 = [1, [2, 3, [4, 5, 6], 7], [8, 9]];
 
 // Q1-1. arr에서 3 출력하기
 // Hint: 인덱싱은 0부터 시작!
-
+console.log(arr[2]);
 // Q1-2. arr에서 [1, 2, 3] 출력하기
 // Hint: slice 함수를 사용해볼까요?
-
+console.log(arr.slice(0,3));
 // Q1-3. arr2에서 [8, 9] 출력하기
 // Hint: 배열 안에 원소가 배열일 뿐!
-
+console.log(arr2[2]);
 // Q1-4. arr2에서 [4, 5, 6] 출력하기
 // Hint: 원소 안의 원소!
-
+console.log(arr2[1][2]);
 console.log("======= 문제 구분선 =======");
 
 // Q2 객체(JSON) 다루기
@@ -62,14 +62,14 @@ const obj = {
 
 // Q2-1. 멋쨍이사자의 나이를 obj 객체로부터 출력하기
 // Hint: obj.~~
-
+console.log(obj.age);
 // Q2-2. 멋쨍이사자의 Computer 학점(A+)을 obj 객체로부터 출력하기
-
+console.log(obj.gpa.Computer);
 // Q2-3. 아기사자 나이(21)를 obj 객체로부터 출력하기
 // Hint: obj 안에 배열
-
+console.log(obj.friends[0].age);
 // Q2-4. 코딩사자가 수강한 강의 중에서 "비즈니스디자인"을 obj 객체로부터 출력하기
-
+console.log(obj.friends[1].lecture[1][2]);
 console.log("======= 문제 구분선 =======");
 
 // Q3 map 함수, filter 함수 활용
@@ -78,10 +78,10 @@ console.log("Q3 map 함수, filter 함수 활용");
 const arr3 = [1, 2, 3, 4, 5, 6];
 
 // Q3-1. map 함수와 arr3을 활용하여 [10, 20, 30, 40, 50, 60]을 출력하시오.
-
+console.log(arr3.map((num)=>num*10));
 // Q3-2. filter 함수와 arr3을 활용하여 [1, 3, 5]를 출력하시오.
 // Hint: 홀수와 짝수 구분은 "% 2"를 활용
-
+console.log(arr3.map((num)=>num%2===1));
 console.log("======= 문제 구분선 =======");
 
 // Q4 함수 & 논리연산자 문제
@@ -104,6 +104,11 @@ console.log(isLeap(2021));  -> 윤년이 아닙니다.
 
 function isLeap(year) {
   // 여기에 코드를 작성하세요.
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return "윤년이 맞습니다.";
+  } else {
+    return "윤년이 아닙니다.";
+  }
 }
 
 console.log(isLeap(2020));
@@ -127,6 +132,9 @@ Hint: for문 or while문 사용 + console.log("*") 사용
 */
 
 // 여기에 코드를 작성하세요.
+for(let i=1; i<=5; i++){
+  console.log("*".repeat(i));
+}
 
 console.log("======= 문제 구분선 =======");
 
@@ -144,7 +152,9 @@ console.log("Q6 반복문 연습문제2");
 
 Hint: console.log(" ")로 빈칸 표현
 */
-
+for(let i=1; i<=5; i++){
+  console.log(" ".repeat(5-i)+"*".repeat(i));
+}
 // 여기에 코드를 작성하세요.
 
 console.log("======= 문제 구분선 =======");
@@ -163,7 +173,9 @@ console.log("Q7 반복문 연습문제3");
 
 Hint: 이중 for문 활용
 */
-
+for(let i=1; i<=5; i++){
+  console.log(" ".repeat(5-i), "*".repeat(2*i-1));
+}
 // 여기에 코드를 작성하세요.
 
 console.log("고생하셨습니다!!");
